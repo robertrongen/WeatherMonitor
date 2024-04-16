f83dc009fde88cf06a5b7752c9ba691245e98ea1
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-16 22:37:10 +0200

    update

---
+++ secrets_example.h
@@ -0,0 +1,9 @@
+// WIFI
+#define STASSID1 "reserve"        // Wifi SSID 1 (case sensitive)
+#define STAPSK1  "reserve"        // WiFi password 1
+#define STASSID2 "reserve"
+#define STAPSK2  "reserve"
+#define STASSID3 "reserve"
+#define STAPSK3  "reserve"
+#define STASSID4 "reserve"
+#define STAPSK4  "reserve"
