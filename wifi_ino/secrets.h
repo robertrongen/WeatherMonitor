6c6616cc0b9091594320a2a8603a02e967137052
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-16 22:32:33 +0200

    Delete wifi_ino/secrets.h

--- wifi_ino/secrets.h
+++
@@ -1,9 +0,0 @@
-// WIFI
-#define STASSID1 "Wodan"           // Wifi SSID 1 (case sensitive)
-#define STAPSK1  "u$UpB22&"        // WiFi password 1
-#define STASSID2 "Betelgeuze"
-#define STAPSK2  "AnnSan4281"
-#define STASSID3 "Orion"
-#define STAPSK3  "smartys!!"
-#define STASSID4 "reserve"
-#define STAPSK4  "reserve"
