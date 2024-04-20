7f8518acac07173646ec04ad4b4b1275a70b512e
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-20 23:01:10 +0200

    adjusted json data handling and added vue support

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def get_db_connection():
 @app.route('/')
 def index():
     conn = get_db_connection()
-    data = conn.execute('SELECT * FROM sky_data ORDER BY timestamp DESC').fetchall()
+    rows = conn.execute('SELECT * FROM sky_data ORDER BY timestamp DESC').fetchall()
+    timestamps = [row['timestamp'] for row in rows]  # Assuming 'timestamp' is a column in your 'sky_data'
     conn.close()
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=rows, timestamps=timestamps)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
