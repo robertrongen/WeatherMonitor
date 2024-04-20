374f6922d2176585f270e0b74ce06dd9acdb79ec
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-20 23:13:25 +0200

    added data store logging

--- store_data.py
+++ store_data.py
@@ -35,53 +35,59 @@ def setup_database():
     conn.close()
 
 def store_sky_data(data):
+    print("Attempting to store data:", data)
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO sky_data (
-            timestamp, 
-            temperature, 
-            humidity, 
-            dew_point, 
-            fan_status, 
-            heater_status,
-            cpu_temperature,
-            raining, 
-            light, 
-            sky_temperature, 
-            ambient_temperature, 
-            sqm_ir, 
-            sqm_full, 
-            sqm_visible, 
-            sqm_lux, 
-            cloud_coverage, 
-            cloud_coverage_indicator, 
-            brightness, 
-            bortle
-        )
-        VALUES (CURRENT_TIMESTAMP, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, (
-        data['temperature'],
-        data['humidity'],
-        data['dew_point'],
-        data['fan_status'],
-        data['heater_status'],
-        data['cpu_temperature'],
-        data['raining'],
-        data['light'],
-        data['sky_temperature'],
-        data['ambient_temperature'],
-        data['sqm_ir'],
-        data['sqm_full'],
-        data['sqm_visible'],
-        data['sqm_lux'],
-        data['cloud_coverage'],
-        data['cloud_coverage_indicator'],
-        data['brightness'],
-        data['bortle']
-    ))
-    conn.commit()
-    conn.close()
+    try:
+        cursor.execute("""
+            INSERT INTO sky_data (
+                timestamp, 
+                temperature, 
+                humidity, 
+                dew_point, 
+                fan_status, 
+                heater_status,
+                cpu_temperature,
+                raining, 
+                light, 
+                sky_temperature, 
+                ambient_temperature, 
+                sqm_ir, 
+                sqm_full, 
+                sqm_visible, 
+                sqm_lux, 
+                cloud_coverage, 
+                cloud_coverage_indicator, 
+                brightness, 
+                bortle
+            )
+            VALUES (CURRENT_TIMESTAMP, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (
+            data['temperature'],
+            data['humidity'],
+            data['dew_point'],
+            data['fan_status'],
+            data['heater_status'],
+            data['cpu_temperature'],
+            data['raining'],
+            data['light'],
+            data['sky_temperature'],
+            data['ambient_temperature'],
+            data['sqm_ir'],
+            data['sqm_full'],
+            data['sqm_visible'],
+            data['sqm_lux'],
+            data['cloud_coverage'],
+            data['cloud_coverage_indicator'],
+            data['brightness'],
+            data['bortle']
+        ))
+        conn.commit()
+        print("Data stored successfully")
+    except Exception as e:
+        print("Failed to store data:", e)
+    finally:
+        conn.close()
 
 if __name__ == "__main__":
     setup_database()  # Set up the database once at the start
