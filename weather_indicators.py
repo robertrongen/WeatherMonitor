dc1b1edeede259e8807275ccf05b8c73ba30b0e0
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-22 20:25:17 +0200

    better  handling of missing data

--- weather_indicators.py
+++ weather_indicators.py
@@ -2,26 +2,30 @@
 from math import log, log10
 
 def calculate_indicators(ambient_temperature, sky_temperature, sqm_lux):
+    cloud_coverage = None
+    cloud_coverage_indicator = None
+    brightness = None
+    bortle = None
+
     try:
         ambient_temperature = float(ambient_temperature)
         sky_temperature = float(sky_temperature)
         sqm_lux = float(sqm_lux)
     except ValueError as e:
         print(f"Error: Invalid input data - {e}")
-        return None, None, None, None
+        return cloud_coverage, cloud_coverage_indicator, brightness, bortle
 
-    if sky_temperature is None or ambient_temperature is None:
-        print("Error: Missing temperature data.")
-        return None, None, None, None
+    if sky_temperature is not None and ambient_temperature is not None:
+        cloud_coverage = (sky_temperature - ambient_temperature) / ambient_temperature
+        cloud_coverage_indicator = ambient_temperature - sky_temperature
+    else:
+        print("Error: Missing sky_temperature or ambient_temperature.")
 
-    if sqm_lux is None or sqm_lux <= 0:  # Check for sqm_lux to be positive
+    if sqm_lux is not None and sqm_lux > 0:
+        brightness = 22.0 - 2.512 * log10(sqm_lux)
+        bortle = 1539.7 * 2.7 ** (-0.28 * brightness)
+    else:
         print("Error: Invalid sqm_lux data.")
-        return None, None, None, None
-
-    cloud_coverage = (sky_temperature - ambient_temperature) / ambient_temperature
-    cloud_coverage_indicator = ambient_temperature - sky_temperature
-    brightness = 22.0 - 2.512 * log10(sqm_lux)
-    bortle = 1539.7 * 2.7 ** (-0.28 * brightness)
 
     return cloud_coverage, cloud_coverage_indicator, brightness, bortle
 
