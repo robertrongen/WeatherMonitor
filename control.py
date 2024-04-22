29b5bdc58fd953928567d6785fc8c4c6d8fa1660
Robert Rongen <robert.rongen@blockchainadvies.nu>
2024-04-22 20:39:11 +0200

    round data values

--- control.py
+++ control.py
@@ -19,18 +19,14 @@ settings = load_settings()  # Initial load of settings
 Relay_Ch1 = 26  # Fan
 Relay_Ch2 = 20  # Dew Heater
 # Relay_Ch3 = 21  # Available for future use
-
-# Set GPIO warnings to false (optional, to avoid nuisance warnings)
-GPIO.setwarnings(False)
+GPIO.setwarnings(False)     # Set GPIO warnings to false (optional, to avoid nuisance warnings)
 GPIO.setmode(GPIO.BCM)
-
-# Setup Relay GPIOs as outputs
 GPIO.setup([Relay_Ch1, Relay_Ch2], GPIO.OUT, initial=GPIO.HIGH)
 
-# Function to control fan and heater based on temperature and humidity
 def control_fan_heater():
+    # Function to control fan and heater and store data
     global settings
-    temp_hum_url = settings["temp_hum_url"]  # Retrieve the URL from settings
+    temp_hum_url = settings["temp_hum_url"]
     settings = load_settings()  # Refresh settings on each call
     if not isinstance(temp_hum_url, str) or 'http' not in temp_hum_url:
         logger.error(f"Invalid URL passed: {temp_hum_url}, using to default url instead")
@@ -44,24 +40,26 @@ def control_fan_heater():
         print("No valid data received.")
 
     if temperature and humidity and serial_data:
-        # Extract required fields from serial_data or other sources
-        ambient_temperature = temperature  # Assuming ambient temperature comes from get_temperature_humidity
-        sky_temperature = serial_data.get('sky_temperature')
-        sqm_lux = serial_data.get('sqm_lux')
-        cloud_coverage, cloud_coverage_indicator, brightness, bortle = calculate_indicators(ambient_temperature, sky_temperature, sqm_lux)
-
-        dew_point = calculate_dew_point(temperature, humidity)
-        dew_point_threshold = temperature - 2
-        # Update fan status logic to include CPU temperature check
+        # Control fan and heater
+        dew_point = round(calculate_dew_point(temperature, humidity), 2)
+        dew_point_threshold = round(temperature - 2, 2)
         fan_status = "ON" if (temperature > settings["ambient_temp_threshold"] or temperature <= dew_point + 1 or cpu_temperature > settings["cpu_temp_threshold"]) else "OFF"
         heater_status = "ON" if temperature <= dew_point_threshold else "OFF"
-        
         GPIO.output(Relay_Ch1, GPIO.LOW if fan_status == "ON" else GPIO.HIGH)
         GPIO.output(Relay_Ch2, GPIO.LOW if heater_status == "ON" else GPIO.HIGH)
 
-        # Package data for storage, including CPU temperature
+        # Get other data, calculate values
+        ambient_temperature = round(temperature, 2)
+        sky_temperature = round(float(serial_data.get('sky_temperature')), 2)
+        sqm_lux = round(float(serial_data.get('sqm_lux')), 2)
+        cloud_coverage, cloud_coverage_indicator, brightness, bortle = calculate_indicators(ambient_temperature, sky_temperature, sqm_lux)
+        cloud_coverage = round(cloud_coverage, 2) if cloud_coverage is not None else None
+        cloud_coverage_indicator = round(cloud_coverage_indicator, 2) if cloud_coverage_indicator is not None else None
+        brightness = round(brightness, 2) if brightness is not None else None
+        bortle = round(bortle, 2) if bortle is not None else None
+        
+        # store data
         conn = sqlite3.connect('sky_data.db')
-
         data = {
             "temperature": temperature,
             "humidity": humidity,
@@ -75,7 +73,6 @@ def control_fan_heater():
             "brightness": brightness,
             "bortle": bortle
         }
-
         logger.debug("Storing data: %s", data)
         store_sky_data(data, conn)
         conn.close()
@@ -88,9 +85,9 @@ if __name__ == '__main__':
     try:
         while True:
             schedule.run_pending()
-            time.sleep(settings["sleep_time"])  # sleep_time could be adjusted to match your timing needs
+            time.sleep(settings["sleep_time"])
     except KeyboardInterrupt:
         logger.info("Program stopped by user")
     finally:
-        GPIO.cleanup()  # Ensure cleanup is called on exit
+        GPIO.cleanup()
         logger.info("GPIO cleanup executed")
