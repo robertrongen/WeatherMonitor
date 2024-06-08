87e82ba4cbc17eeb0acf318b03ee11a26354c74c
Robert Rongen <robert.rongen@ziggo.nl>
2024-06-08 22:28:14 +0200

    removed extra temp read loop

--- control.py
+++ control.py
@@ -1,20 +1,21 @@
 # control.py
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import time
+from settings import load_settings
+from fetch_data import get_temperature_humidity, get_sky_data, get_rain_wind_data, get_cpu_temperature, get_memory_usage
+from weather_indicators import calculate_indicators, calculate_dewPoint
+from meteocalc import heat_index, Temp
+from store_data import store_sky_data, setup_database
+from app_logging import setup_logger
+from rain_alarm import check_rain_alert
+from app import notify_new_data, get_db_connection
+
 try:
     import RPi.GPIO as GPIO
 except (ImportError, RuntimeError):
     print("GPIO library can only be run on a Raspberry Pi, importing mock GPIO")
     GPIO = None
-import time
-from settings import load_settings
-from fetch_data import get_temperature_humidity, get_sky_data, get_rain_wind_data, get_cpu_temperature, get_memory_usage
-from weather_indicators import calculate_indicators, calculate_dewPoint
-from meteocalc import heat_index, Temp #, dew_point
-from store_data import store_sky_data, setup_database
-from app_logging import setup_logger
-from rain_alarm import check_rain_alert
-from app import notify_new_data, get_db_connection
 
 logger = setup_logger('control', 'control.log')
 settings = load_settings()  # Initial load of settings
@@ -29,8 +30,7 @@ if GPIO:
     GPIO.setup([Relay_Ch1, Relay_Ch2], GPIO.OUT, initial=GPIO.HIGH)
 
 def control_fan_heater():
-    # Function to control fan and heater and store data
-    print("controlling fan heater")
+    logger.info("Entering control_fan_heater function")
     global settings
     temp_hum_url = settings["temp_hum_url"]
     settings = load_settings()  # Refresh settings on each call
@@ -138,21 +138,6 @@ def control_fan_heater():
         GPIO.output(Relay_Ch2, GPIO.LOW if data["heater_status"] == "ON" else GPIO.HIGH)
     logger.debug("After GPIO operations")
 
-    # Get other data, calculate values
-    try:
-        if data["temperature"] is not None or data["humidity"] is not None or data["dew_point"] is not None:
-            ambient_temperature = data.get("temperature")
-            sky_temperature = round(float(data.get('sky_temperature')), 1) if data.get('sky_temperature') else None
-            sqm_lux = round(float(data.get('sqm_lux')), 2) if data.get('sqm_lux') else None
-            cloud_coverage, cloud_coverage_indicator, brightness, bortle = calculate_indicators(ambient_temperature, sky_temperature, sqm_lux)
-            data["cloud_coverage"] = round(cloud_coverage, 2) if cloud_coverage is not None else None
-            data["cloud_coverage_indicator"] = round(cloud_coverage_indicator, 1) if cloud_coverage_indicator is not None else None
-            data["brightness"] = round(brightness, 1) if brightness is not None else None
-            data["bortle"] = round(bortle, 1) if bortle is not None else None
-            logger.info(f"Computed additional indicators: cloud_coverage: {data['cloud_coverage']}, brightness: {data['brightness']}, bortle: {data['bortle']}")
-    except Exception as e:
-        logger.error(f"Failed to calculate additional indicators: {e}")
-
     # store data
     logger.info("Storing data: %s", data)
     conn = get_db_connection()
@@ -167,13 +152,15 @@ def control_fan_heater():
 if __name__ == '__main__':
     setup_database()
     try:
+        logger.info("Starting control loop")
         control_fan_heater()
         while True:
+            logger.debug("Sleeping for interval")
             time.sleep(settings["sleep_time"])
+            logger.debug("Calling control_fan_heater()")
             control_fan_heater()
     except KeyboardInterrupt:
         logger.info("Program stopped by user")
     finally:
         if GPIO:
             GPIO.cleanup()
-            # logger.info("GPIO cleanup executed")
